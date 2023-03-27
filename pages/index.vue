0101e268b547421249efaee1f7bacc65c432f6d2
Mary Sennello <marysenn@uw.edu>
2023-03-26 18:13:00 -0700

    added pages folder

---
+++ pages/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <!--head, header and nav-->
+    <HeaderNav />
+
+  <!--VIEWS ROUTED IN HERE-->
+    <HomeView />
+
+  <!--footer for every page-->
+    <FooterSection />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+
+
+<style>
+@import "@/assets/css/style.css";
+
+#app {
+  //font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  displaalign-content: stretch;
+  justify-content: space-around;
+  width: 100%;
+}
+y: flex;
+  flex-wrap: wrap;
+  
+.headernav, footer, nav {
+  width: 100%;
+}
+
+nav {
+  padding: 30px;
+  width: 100%;
+}
+nav a {
+    color: #2c3e50;
+  text-decoration: none;
+  padding: 1em 2em;
+}
+a:hover {
+  text-decoration: none;
+  color: #fdffe0;
+  font-weight: 500;
+}
+router-link-exact-active {
+  color: #536f50;
+}
+
+</style>
